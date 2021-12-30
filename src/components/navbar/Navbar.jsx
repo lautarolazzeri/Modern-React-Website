fa44552f2b104d6624def6f858e73c1c7d93d204
Lautaro <lautyylazzerii@gmail.com>
2021-12-30 11:30:08 -0300

    assets

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,10 +1,18 @@
 import React from 'react';
+import logo from '../../assets/logo.svg';
 import './navbar.css';
+import { RiMenu3Line, RiCloseLin } from 'react-icons/ri';
+
+
 
 function Navbar() {
     return (
-        <div>
-            Navbar
+        <div className='gpt3__navbar'>
+            <div className="gpt3__navbar-links">
+                <div className="gpt3__navbar-links_logo">
+                    <img src="{logo}" alt="Logo" />
+                </div>
+            </div>
         </div>
     )
 }
