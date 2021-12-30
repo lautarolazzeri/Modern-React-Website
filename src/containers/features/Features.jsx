0da9a2664c18331a34ab3702d57e5c507ac34d2e
Lautaro <lautyylazzerii@gmail.com>
2021-12-30 10:34:14 -0300

    first commit

---
+++ src/containers/features/Features.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './features.css';
+
+function Features() {
+    return (
+        <div>
+            Features
+        </div>
+    )
+}
+
+export default Features
