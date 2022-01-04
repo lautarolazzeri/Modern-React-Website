09eae979d99af63e9f782cdb84fe1a9f2fc5bab9
Lautaro <lautyylazzerii@gmail.com>
2022-01-04 12:42:25 -0300

    cta section responsive

--- src/components/cta/CTA.jsx
+++ src/components/cta/CTA.jsx
@@ -5,7 +5,7 @@ function Cta() {
     return (
         <div>
             <div className="gpt3__cta">
-                <div className="gpt3__cta-container">
+                <div className="gpt3__cta-content">
                     <p>Request Early Access to Get Started</p>
                     <h3>Register today & start exploring the endless possiblities.</h3>
                 </div>
@@ -16,5 +16,5 @@ function Cta() {
         </div>
     )
 }
-
+ 
 export default Cta
