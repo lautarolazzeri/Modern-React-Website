8a59a56ab50db64a5f56f06f8249ef04f5ed472c
Lautaro <lautyylazzerii@gmail.com>
2022-01-06 14:07:42 -0300

    FInal Commit, finished Website

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ const App = () => {
                 <Navbar />
                 <Header />
             </div>
+            
             <Brand />
             <WhatGPT3 />
             <Features />
